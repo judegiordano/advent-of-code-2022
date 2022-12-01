be2750e1f01c06664d4755b3a004eacf1328cabd
judegiordano <judegiordano@gmail.com>
2022-12-01 12:43:44 -0500

    Tests

--- src/day_1.rs
+++ src/day_1.rs
@@ -8,7 +8,6 @@ use rayon::{
     slice::ParallelSliceMut,
 };
 
-// answer 69626
 fn part1(input: &str) -> Result<Vec<(u64, u64)>> {
     let mut calories_by_elf: BTreeMap<u64, u64> = BTreeMap::new();
     let mut elf = 0;
@@ -30,23 +29,43 @@ fn part1(input: &str) -> Result<Vec<(u64, u64)>> {
     }
     let mut v = Vec::from_par_iter(calories_by_elf);
     v.par_sort_by(|&(_, a), &(_, b)| b.cmp(&a));
-    match v.first() {
-        Some((elf, calories)) => println!("top calories: {calories:?} held by elf {elf:?}"),
-        None => anyhow::bail!("empty"),
-    }
     Ok(v)
 }
 
-// answer 206780
-fn part2(input: &str) -> Result<()> {
-    let sorted_list = part1(input)?;
+fn part2(sorted_list: &[(u64, u64)]) -> Result<u64> {
     let top_3 = sorted_list.par_iter().take(3).collect::<Vec<_>>();
     let total = top_3.iter().fold(0, |acc, (_, calories)| acc + calories);
-    println!("total calories held by top 3 elves: {total:?}");
-    Ok(())
+    Ok(total)
 }
 
 pub fn run(input: &str) -> Result<()> {
-    part2(input)?;
+    let sorted_list = part1(input)?;
+    match sorted_list.first() {
+        Some((elf, calories)) => println!("top calories: {calories:?} held by elf {elf:?}"),
+        None => anyhow::bail!("empty"),
+    }
+    let top_3_total = part2(&sorted_list)?;
+    println!("total calories held by top 3 elves: {top_3_total:?}");
     Ok(())
+}
+
+#[allow(unused_imports)]
+#[cfg(test)]
+pub mod tests {
+
+    use super::*;
+    use crate::helpers::read_inputs_txt;
+
+    #[test]
+    fn day1_tests() -> Result<()> {
+        let input = read_inputs_txt("day1")?;
+        let sorted_list = part1(&input)?;
+        match sorted_list.first() {
+            Some((_, calories)) => assert_eq!(*calories, 69626),
+            None => anyhow::bail!("empty"),
+        }
+        let top_3_total = part2(&sorted_list)?;
+        assert_eq!(top_3_total, 206780);
+        Ok(())
+    }
 }
