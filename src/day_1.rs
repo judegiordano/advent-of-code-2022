929b936dd8115d7ee4b3f39be130716348799bab
judegiordano <judegiordano@gmail.com>
2022-12-01 12:15:17 -0500

    Clap / Optimizations

--- src/day_1.rs
+++ src/day_1.rs
@@ -1,43 +1,52 @@
 use std::collections::BTreeMap;
 
 use anyhow::Result;
+use rayon::{
+    prelude::{
+        FromParallelIterator, IndexedParallelIterator, IntoParallelRefIterator, ParallelIterator,
+    },
+    slice::ParallelSliceMut,
+};
 
-use crate::helpers::read_inputs_txt;
-
-fn part1() -> Result<Vec<(u64, u64)>> {
-    let content = read_inputs_txt("day1")?;
+// answer 69626
+fn part1(input: &str) -> Result<Vec<(u64, u64)>> {
     let mut calories_by_elf: BTreeMap<u64, u64> = BTreeMap::new();
     let mut elf = 0;
-    for a in content.lines() {
-        if a.len() == 0 {
+    for a in input.lines() {
+        // whitespace in input used as separator
+        if a.is_empty() {
+            // new elf
             elf += 1;
             continue;
         }
         let current_value = calories_by_elf.get(&elf);
         let value = a.parse::<u64>()?;
-        if current_value.is_some() {
-            calories_by_elf.insert(elf, current_value.unwrap() + value);
-        } else {
-            calories_by_elf.insert(elf, value);
-        }
+        match current_value {
+            // update elf total count if exists
+            Some(current) => calories_by_elf.insert(elf, current + value),
+            // create new elf
+            None => calories_by_elf.insert(elf, value),
+        };
     }
-    let mut v = Vec::from_iter(calories_by_elf);
-    v.sort_by(|&(_, a), &(_, b)| b.cmp(&a));
-    let (elf, calories) = v.first().unwrap();
-    println!("top calories: {:#?} held by elf {:?}", calories, elf);
+    let mut v = Vec::from_par_iter(calories_by_elf);
+    v.par_sort_by(|&(_, a), &(_, b)| b.cmp(&a));
+    match v.first() {
+        Some((elf, calories)) => println!("top calories: {calories:?} held by elf {elf:?}"),
+        None => anyhow::bail!("empty"),
+    }
     Ok(v)
 }
 
-fn part2() -> Result<()> {
-    let sorted_list = part1()?;
-    let top_3 = sorted_list.iter().take(3).collect::<Vec<_>>();
+// answer 206780
+fn part2(input: &str) -> Result<()> {
+    let sorted_list = part1(input)?;
+    let top_3 = sorted_list.par_iter().take(3).collect::<Vec<_>>();
     let total = top_3.iter().fold(0, |acc, (_, calories)| acc + calories);
-    println!("total calories held by top 3 elves: {:#?}", total);
+    println!("total calories held by top 3 elves: {total:?}");
     Ok(())
 }
 
-pub fn run() -> Result<()> {
-    // part1()?;
-    part2()?;
+pub fn run(input: &str) -> Result<()> {
+    part2(input)?;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
+use clap::Parser;
+
+use crate::helpers::read_inputs_txt;
+
 pub mod day_1;
 pub mod helpers;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// input file name no extension
+    #[arg(short, long)]
+    path: String,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let Args { path } = Args::parse();
+    let content = read_inputs_txt(&path)?;
     let start = std::time::Instant::now();
-    day_1::run()?;
+    day_1::run(&content)?;
     println!("operation complete in: {:#?}", start.elapsed());
     Ok(())
 }
