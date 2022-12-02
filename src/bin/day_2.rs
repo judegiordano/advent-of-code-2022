7254d188239c578338a2da267f7b314b712c9d3d
judegiordano <judegiordano@gmail.com>
2022-12-02 16:46:40 -0500

    Cleanup

--- src/bin/day_2.rs
+++ src/bin/day_2.rs
@@ -17,6 +17,11 @@ const INPUT: &str = include_str!("../../inputs/day2.txt");
 // Y -> draw
 // Z -> win
 
+enum GameOutput {
+    Part1((Attack, Attack)),
+    Part2((Attack, GameOutcome)),
+}
+
 #[derive(Debug, Clone)]
 enum Attack {
     Rock,
@@ -25,7 +30,7 @@ enum Attack {
 }
 
 #[derive(Debug, Clone)]
-enum Outcome {
+enum GameOutcome {
     Win,
     Lose,
     Draw,
@@ -39,136 +44,161 @@ trait Game {
     fn game_tally(&self) -> u32;
 }
 
-impl Round for (Attack, Attack) {
+trait Part1Key {
+    fn to_opponent_attack(&self) -> Attack;
+    fn to_my_attack(&self) -> Attack;
+}
+
+trait Part2Key {
+    fn to_game_result(&self) -> GameOutcome;
+}
+
+impl Round for GameOutput {
     fn round_score(&self) -> u32 {
         let mut score = 0;
-        let (their_attack, my_attack) = self;
-        match my_attack {
-            Attack::Rock => score += 1,
-            Attack::Paper => score += 2,
-            Attack::Scissors => score += 3,
-        }
-        match their_attack {
-            Attack::Rock => match my_attack {
-                Attack::Rock => score += 3,     // tie
-                Attack::Paper => score += 6,    // win
-                Attack::Scissors => score += 0, // loss
-            },
-            Attack::Paper => match my_attack {
-                Attack::Rock => score += 0,     // loss
-                Attack::Paper => score += 3,    // tie
-                Attack::Scissors => score += 6, // win
-            },
-            Attack::Scissors => match my_attack {
-                Attack::Rock => score += 6,     // win
-                Attack::Paper => score += 0,    // loss
-                Attack::Scissors => score += 3, // tie
-            },
+        match self {
+            // per part1 requirements, tally score based on attack vs attack
+            Self::Part1(input) => {
+                let (their_attack, my_attack) = input;
+                // modify base score based on what i threw
+                match my_attack {
+                    Attack::Rock => score += 1,
+                    Attack::Paper => score += 2,
+                    Attack::Scissors => score += 3,
+                }
+                // match opponent attack to outcome
+                match their_attack {
+                    Attack::Rock => match my_attack {
+                        Attack::Rock => score += 3,     // draw
+                        Attack::Paper => score += 6,    // win
+                        Attack::Scissors => score += 0, // loss
+                    },
+                    Attack::Paper => match my_attack {
+                        Attack::Rock => score += 0,     // loss
+                        Attack::Paper => score += 3,    // draw
+                        Attack::Scissors => score += 6, // win
+                    },
+                    Attack::Scissors => match my_attack {
+                        Attack::Rock => score += 6,     // win
+                        Attack::Paper => score += 0,    // loss
+                        Attack::Scissors => score += 3, // draw
+                    },
+                }
+            }
+            // per part2 requirements, tally score based on attack -> game result
+            Self::Part2(input) => {
+                let (their_attack, outcome) = input;
+                match their_attack {
+                    Attack::Rock => match outcome {
+                        GameOutcome::Win => score += 8,  // paper + win
+                        GameOutcome::Lose => score += 3, // scissors + loss
+                        GameOutcome::Draw => score += 4, // rock + draw
+                    },
+                    Attack::Paper => match outcome {
+                        GameOutcome::Win => score += 9,  // scissors + win
+                        GameOutcome::Lose => score += 1, // rock + loss
+                        GameOutcome::Draw => score += 5, // paper + draw
+                    },
+                    Attack::Scissors => match outcome {
+                        GameOutcome::Win => score += 7,  // rock + win
+                        GameOutcome::Lose => score += 2, // paper + loss
+                        GameOutcome::Draw => score += 6, // scissors + draw
+                    },
+                }
+            }
         }
         score
     }
 }
 
-impl Game for Vec<(Attack, Attack)> {
+impl Game for Vec<GameOutput> {
+    // tally entire game score given input of games
     fn game_tally(&self) -> u32 {
         self.into_par_iter()
-            .fold(|| 0, |total, round| total + round.round_score())
+            .map(Round::round_score)
+            .fold(|| 0, |total, round| total + round)
             .sum::<u32>()
     }
 }
 
-impl Round for (Attack, Outcome) {
-    fn round_score(&self) -> u32 {
-        let mut score = 0;
-        let (their_attack, outcome) = self;
-        match their_attack {
-            Attack::Rock => match outcome {
-                Outcome::Win => score += 8,  // paper + win
-                Outcome::Lose => score += 3, // scissors + loss
-                Outcome::Draw => score += 4, // rock + draw
-            },
-            Attack::Paper => match outcome {
-                Outcome::Win => score += 9,  // scissors + win
-                Outcome::Lose => score += 1, // rock + loss
-                Outcome::Draw => score += 5, // paper + draw
-            },
-            Attack::Scissors => match outcome {
-                Outcome::Win => score += 7,  // rock + win
-                Outcome::Lose => score += 2, // paper + loss
-                Outcome::Draw => score += 6, // scissors + draw
-            },
-        }
-        score
-    }
-}
-
-impl Game for Vec<(Attack, Outcome)> {
-    fn game_tally(&self) -> u32 {
-        self.into_par_iter()
-            .fold(|| 0, |total, round| total + round.round_score())
-            .sum::<u32>()
-    }
-}
-
-fn part1() -> Result<Vec<(Attack, Attack)>> {
-    let mut rounds = vec![];
-    for line in INPUT.lines() {
-        let throws = &line.split(' ').collect::<Vec<_>>()[..];
-        let their_attack = match throws.first() {
+impl Part1Key for Option<&String> {
+    // map an opponents given input to a logical attack enum
+    fn to_opponent_attack(&self) -> Attack {
+        match *self {
             Some(play) => match play.trim().to_uppercase().as_ref() {
                 "A" => Attack::Rock,
                 "B" => Attack::Paper,
                 "C" => Attack::Scissors,
-                _ => anyhow::bail!("not supported"),
+                _ => panic!("handle this"),
             },
-            None => anyhow::bail!("not supported"),
-        };
-        let my_attack = match throws.last() {
+            None => panic!("handle this"),
+        }
+    }
+
+    // map my given input to a logical attack enum
+    fn to_my_attack(&self) -> Attack {
+        match self {
             Some(play) => match play.trim().to_uppercase().as_ref() {
                 "X" => Attack::Rock,
                 "Y" => Attack::Paper,
                 "Z" => Attack::Scissors,
-                _ => anyhow::bail!("not supported"),
+                _ => panic!("handle this"),
             },
-            None => anyhow::bail!("not supported"),
-        };
-        rounds.push((their_attack, my_attack))
+            None => panic!("handle this"),
+        }
     }
-    Ok(rounds)
 }
 
-fn part2() -> Result<Vec<(Attack, Outcome)>> {
+impl Part2Key for Option<&String> {
+    fn to_game_result(&self) -> GameOutcome {
+        match *self {
+            Some(play) => match play.trim().to_uppercase().as_ref() {
+                "X" => GameOutcome::Lose,
+                "Y" => GameOutcome::Draw,
+                "Z" => GameOutcome::Win,
+                _ => panic!("handle this"),
+            },
+            None => panic!("handle this"),
+        }
+    }
+}
+
+fn part1() -> Vec<GameOutput> {
     let mut rounds = vec![];
     for line in INPUT.lines() {
-        let throws = &line.split(' ').collect::<Vec<_>>()[..];
-        let their_attack = match throws.first() {
-            Some(play) => match play.trim().to_uppercase().as_ref() {
-                "A" => Attack::Rock,
-                "B" => Attack::Paper,
-                "C" => Attack::Scissors,
-                _ => anyhow::bail!("not supported"),
-            },
-            None => anyhow::bail!("not supported"),
-        };
-        let my_outcome = match throws.last() {
-            Some(play) => match play.trim().to_uppercase().as_ref() {
-                "X" => Outcome::Lose,
-                "Y" => Outcome::Draw,
-                "Z" => Outcome::Win,
-                _ => anyhow::bail!("not supported"),
-            },
-            None => anyhow::bail!("not supported"),
-        };
-        rounds.push((their_attack, my_outcome))
+        let line = line.to_string();
+        let throws = line
+            .split(' ')
+            .into_iter()
+            .map(|a| a.to_string())
+            .collect::<Vec<String>>();
+        let their_attack = throws.first().to_opponent_attack();
+        let my_attack = throws.last().to_my_attack();
+        rounds.push(GameOutput::Part1((their_attack, my_attack)));
     }
-    Ok(rounds)
+    rounds
+}
+
+fn part2() -> Vec<GameOutput> {
+    let mut rounds = vec![];
+    for line in INPUT.lines() {
+        let throws = line
+            .split(' ')
+            .into_iter()
+            .map(|a| a.to_string())
+            .collect::<Vec<String>>();
+        let their_attack = throws.first().to_opponent_attack();
+        let my_outcome = throws.last().to_game_result();
+        rounds.push(GameOutput::Part2((their_attack, my_outcome)));
+    }
+    rounds
 }
 
 pub fn main() -> Result<()> {
     let start = std::time::Instant::now();
-    let total = part1()?.game_tally();
+    let total = part1().game_tally();
     println!("initial game info total: {total:#?}");
-    let total = part2()?.game_tally();
+    let total = part2().game_tally();
     println!("final game info total: {total:#?}");
     println!("operation complete in: {:#?}", start.elapsed());
     Ok(())
@@ -182,9 +212,9 @@ pub mod tests {
 
     #[test]
     fn day2_tests() -> Result<()> {
-        let total = part1()?.game_tally();
+        let total = part1().game_tally();
         assert_eq!(total, 15691);
-        let total = part2()?.game_tally();
+        let total = part2().game_tally();
         assert_eq!(total, 12989);
         Ok(())
     }
